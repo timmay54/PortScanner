7bd03a8a3a43a9d4b7c00e69784e92d2a1837002
timmay54 <timothymardesen@yahoo.com>
2018-04-17 22:18:57 -0500

    Adding more content, new Terms of Agreements warning

--- PortScanner.py
+++ PortScanner.py
@@ -6,11 +6,25 @@ Tim's python Port Scanner written in python
 import os
 import socket
 
+wontBlameMe = False #Terms and agreement is by default accepted
+loop = True         #will break nested loops when needed
 
 def initialization():
-	print(os.uname())
+	print os.uname()
+	print()
 
-
+def termsOfAgreement():
+	print "By running this program, you are acknowledging that what you are doing is probably illegal, and that this tool is to be used in an educational manner only. I am in no way responsible for you getting into legal trouble for using this application in an ill manner."
+	ans = input("1. Disagree. You need a scapegoat to blame.\n99.Agree to not blaming me!")
+	if (ans = 1):
+		print "Shame, get outta here."
+		loop = false
+	elif (ans = 99):
+		print "Terrific, lets get scanning!!"
+		wontBlameMe = True
 
 initialization()
 
+def startMenu():
+	print "Welcome to Tim's Port Scanner. You must first accept\nthe terms and agreements before you begin"
+	termsOfAgreement()  #possible, if tOa not true, loop it until it is??
